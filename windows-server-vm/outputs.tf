3f7d270fccf350c80e36e6050c945947274eb2ee
Praveen Kumar Reddy Kona <86263457+praveenkumarreddykona@users.noreply.github.com>
2023-07-30 22:46:02 +0530

    updated references

--- windows-server-vm/outputs.tf
+++ windows-server-vm/outputs.tf
@@ -1,34 +1,34 @@
 output "resource_group_id" {
   description = "ID of the created resource group."
-  value       = azurerm_resource_group.example_rg.id
+  value       = azurerm_resource_group.rg.id
 }
 
 output "key_vault_id" {
   description = "ID of the created Key Vault."
-  value       = azurerm_key_vault.example_kv.id
+  value       = azurerm_key_vault.vault.id
 }
 
 output "nsg_id" {
   description = "ID of the created Network Security Group."
-  value       = azurerm_network_security_group.example_nsg.id
+  value       = azurerm_network_security_group.nsg.id
 }
 
 output "nic_id" {
   description = "ID of the created Network Interface."
-  value       = azurerm_network_interface.example_nic.id
+  value       = azurerm_network_interface.nic.id
 }
 
 output "vnet_id" {
   description = "ID of the created Virtual Network."
-  value       = azurerm_virtual_network.example_vnet.id
+  value       = azurerm_virtual_network.vnet.id
 }
 
 output "public_ip_id" {
   description = "ID of the created Public IP Address."
-  value       = azurerm_public_ip.example_pip.id
+  value       = azurerm_public_ip.pip.id
 }
 
 output "vm_id" {
   description = "ID of the created Virtual Machine."
-  value       = azurerm_virtual_machine.example_vm.id
+  value       = azurerm_virtual_machine.vm.id
 }
